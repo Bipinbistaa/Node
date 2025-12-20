d19ecbc17c755e81846881b0961aec1ff031646d
Bipin Bista <bipinbista@Bipins-MacBook-Air.local>
2025-12-20 21:26:48 +0545

    Day4

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const dbSangaconnectHu = require("./Database/connection")
 const User = require("./models/userModel")
 const Blog = require("./models/blogModule")
 const app = express()
+const bcrypt = require("bcrypt")
+
 
 dbSangaconnectHu()
+app.use(express.json()) //used to display json data from the postman
 
 app.get("/",function(req,res){
     res.send("Hello World")
@@ -42,6 +45,26 @@ app.get("/",function(req,res){
     
 })
 
+app.post("/register",async function(req,res){
+    // console.log(req.body)
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password //const {name ,email ,password} = req.body which is known as destructuring
+    console.log(name,email,password)
+
+    await User.create({
+        name : name,
+        email : email,
+        password : bcrypt.hashSync(password,10) //10 means it hash the encrypted data for 10 times more this number more the secure
+        
+
+    })
+    res.json({
+        message : "User registered successfully!!"
+    })
+
+})
+
 app.get("/fetch-blog",async function(req,res){
     const data = await Blog.find()
     res.json({
@@ -50,12 +73,58 @@ app.get("/fetch-blog",async function(req,res){
     
 })
 
+// app.delete("/delete/:id",async function(req,res){
+//     const id = req.params.id
+//     await User.findByIdAndDelete(id)
+//     res.json({
+//         message : "User with that id deleted successfully!!"
+//     })
+// })
+app.delete("/delete",async function(req,res){
+    const {id} = req.body
+    await User.findByIdAndDelete(id)
 
-app.listen(3000,function(){
-    console.log("server has started at port 3000")
-   
-
+    res.json({
+        message : "User with that id deleted successfully!!"
+    })
 })
 
 
-// mongodb+srv://Nodejs:<db_password>@cluster0.oxgk79c.mongodb.net/?appName=Cluster0
+//create blog 
+
+app.post("/createBlog",async function(req,res){
+
+    
+    const title  = req.body.title
+    const subtitle  = req.body.subtitle
+    const description  = req.body.description
+    console.log(title,subtitle,description)
+
+    await Blog.create({
+        title,
+        subtitle,
+        description        
+
+    })
+    res.json({
+        message : "blog registered successfully!!"
+    })
+
+})
+
+//delete blog
+
+app.delete("/deleteblog/:id",async function(req,res){
+    const id = req.params.id
+    await Blog.findByIdAndDelete(id)
+    res.json({
+        message : "Blog with that id deleted successfully!!"
+    })
+})
+
+
+app.listen(3000,function(){
+    console.log("server has started at port 3000")
+})
+
+
